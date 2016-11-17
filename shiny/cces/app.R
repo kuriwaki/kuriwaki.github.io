da405b7643a10156002f420aff1ec7b950d22c0e
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2016-11-17 13:11:50 -0500

    new vars and working shiny code

--- shiny/cces/app.R
+++ shiny/cces/app.R
@@ -8,43 +8,69 @@
 #
 
 library(shiny)
+library(foreign)
+library(dplyr)
+library(ggplot2)
 
 # Define UI for application that draws a histogram
 ui <- shinyUI(fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("2012 CCES Data"),
    
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+   selectInput("xvar", "X Variable:",
+               c("Age" = "age",
+                 "Survey Weight" = "weight",
+                 "Gender" = "gender",
+                 "Race" = "race",
+                 "Employment Status" = "employ",
+                 "Family Income" = "family_income",
+                 "Religion" = "religion",
+                 "Obama Vote" = "obama12")),
+   selectInput("yvar", "Y Variable:",
+               c("Survey Weight" = "weight",
+                 "Age" = "age",
+                 "Gender" = "gender",
+                 "Race" = "race",
+                 "Employment Status" = "employ",
+                 "Family Income" = "family_income",
+                 "Religion" = "religion",
+                 "Obama Vote" = "obama12")),
+   selectInput("colorvar", "Color Variable:",
+               c("Obama Vote" = "obama12",
+                 "Survey Weight" = "weight",
+                 "Age" = "age",
+                 "Gender" = "gender",
+                 "Race" = "race",
+                 "Employment Status" = "employ",
+                 "Family Income" = "family_income",
+                 "Religion" = "religion")),
+   selectInput("facetvar", "Facet Variable:",
+               c("State" = "state",
+                 "Gender" = "gender",
+                 "Race" = "race",
+                 "Employment Status" = "employ",
+                 "Family Income" = "family_income",
+                 "Religion" = "religion",
+                 "Obama Vote" = "obama12")),
+   plotOutput("data")
 ))
 
 # Define server logic required to draw a histogram
 server <- shinyServer(function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  # connect to data
+  # db <- src_sqlite("https://kuriwaki.github.io/datasets/cces.sqlite3")
+     cc <- foreign::read.dta("https://kuriwaki.github.io/datasets/cces_sample.dta")
+  
+   output$data <- renderPlot({
+     ggplot(cc, aes_string(x = input$xvar, y = input$yvar, color = input$colorvar)) +
+       geom_point(alpha = 0.7) +
+       facet_wrap(input$facetvar) +
+       geom_smooth(method = "gam", color = "navy") + 
+       theme_minimal()
    })
 })
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-

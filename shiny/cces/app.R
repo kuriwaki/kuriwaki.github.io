92b36dde3fd9cbb9b66dffc1b416136ffa66f440
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2016-11-17 13:36:15 -0500

    more features

--- shiny/cces/app.R
+++ shiny/cces/app.R
@@ -14,47 +14,78 @@ library(ggplot2)
 
 # Define UI for application that draws a histogram
 ui <- shinyUI(fluidPage(
-   
-   # Application title
-   titlePanel("2012 CCES Data"),
-   
-   selectInput("xvar", "X Variable:",
-               c("Age" = "age",
-                 "Survey Weight" = "weight",
-                 "Gender" = "gender",
-                 "Race" = "race",
-                 "Employment Status" = "employ",
-                 "Family Income" = "family_income",
-                 "Religion" = "religion",
-                 "Obama Vote" = "obama12")),
-   selectInput("yvar", "Y Variable:",
-               c("Survey Weight" = "weight",
-                 "Age" = "age",
-                 "Gender" = "gender",
-                 "Race" = "race",
-                 "Employment Status" = "employ",
-                 "Family Income" = "family_income",
-                 "Religion" = "religion",
-                 "Obama Vote" = "obama12")),
-   selectInput("colorvar", "Color Variable:",
-               c("Obama Vote" = "obama12",
-                 "Survey Weight" = "weight",
-                 "Age" = "age",
-                 "Gender" = "gender",
-                 "Race" = "race",
-                 "Employment Status" = "employ",
-                 "Family Income" = "family_income",
-                 "Religion" = "religion")),
-   selectInput("facetvar", "Facet Variable:",
-               c("State" = "state",
-                 "Gender" = "gender",
-                 "Race" = "race",
-                 "Employment Status" = "employ",
-                 "Family Income" = "family_income",
-                 "Religion" = "religion",
-                 "Obama Vote" = "obama12")),
-   plotOutput("data")
-))
+  
+  # Application title
+  titlePanel("2012 CCES Data"),
+  helpText("Use this app to browse the results of the 2012 CCES survey"),
+  
+  # Layout
+  sidebarLayout(
+    
+    # Sidebar
+    sidebarPanel(
+      selectInput("xvar", "X Variable:",
+                  c("Age" = "age",
+                    "Survey Weight" = "weight",
+                    "Gender" = "gender",
+                    "Race" = "race",
+                    "Employment Status" = "employ",
+                    "Family Income" = "family_income",
+                    "Religion" = "religion",
+                    "Home Ownership" = "home_ownership",
+                    "Union Membership" = "union",
+                    "Partisan Identification (3-point)" = "pid3",
+                    "Ideology (5-point)" = "ideo5",
+                    "Ideology (7-point)" = "ideo7",
+                    "Obama Vote" = "obama12")),
+      selectInput("yvar", "Y Variable:",
+                  c("Survey Weight" = "weight",
+                    "Age" = "age",
+                    "Gender" = "gender",
+                    "Race" = "race",
+                    "Employment Status" = "employ",
+                    "Family Income" = "family_income",
+                    "Religion" = "religion",
+                    "Home Ownership" = "home_ownership",
+                    "Union Membership" = "union",
+                    "Partisan Identification (3-point)" = "pid3",
+                    "Ideology (5-point)" = "ideo5",
+                    "Ideology (7-point)" = "ideo7",
+                    "Presidential Vote" = "vote_pres_12")),
+      selectInput("colorvar", "Color Variable:",
+                  c("Partisan Identification (3-point)" = "pid3",
+                    "Presidential Vote" = "vote_pres_12",
+                    "Survey Weight" = "weight",
+                    "Age" = "age",
+                    "Gender" = "gender",
+                    "Race" = "race",
+                    "Employment Status" = "employ",
+                    "Family Income" = "family_income",
+                    "Home Ownership" = "home_ownership",
+                    "Union Membership" = "union",
+                    "Ideology (5-point)" = "ideo5",
+                    "Ideology (7-point)" = "ideo7",
+                    "Religion" = "religion")),
+      selectInput("facetvar", "Facet Variable:",
+                  c("Presidential Vote" = "vote_pres_12",
+                    "State" = "state",
+                    "Gender" = "gender",
+                    "Race" = "race",
+                    "Employment Status" = "employ",
+                    "Family Income" = "family_income",
+                    "Religion" = "religion",
+                    "Home Ownership" = "home_ownership",
+                    "Union Membership" = "union",
+                    "Partisan Identification (3-point)" = "pid3",
+                    "Ideology (5-point)" = "ideo5",
+                    "Ideology (7-point)" = "ideo7"))
+    ), # end sidebar
+    
+    # Graph
+    mainPanel(plotOutput("data", height = "800px"))
+  ) # end Layout
+)
+)
 
 # Define server logic required to draw a histogram
 server <- shinyServer(function(input, output) {
@@ -65,7 +96,8 @@ server <- shinyServer(function(input, output) {
   
    output$data <- renderPlot({
      ggplot(cc, aes_string(x = input$xvar, y = input$yvar, color = input$colorvar)) +
-       geom_point(alpha = 0.7) +
+       geom_point(alpha = 0.5) +
+       scale_color_brewer(palette = "Dark2") +
        facet_wrap(input$facetvar) +
        geom_smooth(method = "gam", color = "navy") + 
        theme_minimal()
